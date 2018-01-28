7b71eab4b090a551c99a8429950c156b08e85935
katherine55 <league@WTS10.attlocal.net>
2018-01-28 11:26:52 -0800

    new
    reverse greeting

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -8,23 +8,26 @@ public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One
 	// robot should draw one ring simultaneously with the other 4 robots.
 public static void main(String[] args) {
-	Robot timmy = new Robot(400, 700);
-	Robot tammy = new Robot(800, 700);
-	Robot llama = new Robot(1200, 700);
+	Robot timmy = new Robot(425, 700);
+	Robot tammy = new Robot(775, 700);
+	Robot llama = new Robot(250, 500);
+	Robot sammy = new Robot(600, 500);
+	Robot robotic = new Robot(950, 500);
 	
-	OlympicRings_Threaded us = new OlympicRings_Threaded();
+	timmy.setSpeed(20);
+	tammy.setSpeed(20);
+	llama.setSpeed(20);	
+	sammy.setSpeed(20);
+	robotic.setSpeed(20);
 	
-	
-	timmy.setSpeed(10);
-	tammy.setSpeed(10);
-	llama.setSpeed(10);	
-	us.drawCircle(timmy, Color.yellow);
-	us.drawCircle(tammy, Color.green);
-//	us.drawCircle(llama, Color.blue);
-	
+	new Thread(()->drawCircle(timmy, Color.yellow)).start();
+	new Thread(()->drawCircle(tammy, Color.green)).start();
+	new Thread(()->drawCircle(llama, Color.blue)).start();
+	new Thread(()->drawCircle(sammy, Color.black)).start();
+	new Thread(()->drawCircle(robotic, Color.red)).start();
 }
 
-	void drawCircle(Robot x, Color y) {
+	static void drawCircle(Robot x, Color y) {
 		x.penDown();
 		x.setPenWidth(20);
 		x.setPenColor(y);
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,7 +1,25 @@
+package threads;
 
 public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
+	// Write a program that creates a thread (Thread 1) that creates another thread
+	// (Thread 2);
+	// Thread 2 creates Thread 3; and so on, up to Thread 50.
+	// Each thread should print "Hello from Thread <num>!",
+	// but you should structure your program such that the threads print their
+	// greetings in reverse order.
+	public static void main(String[] args) {
+		ThreadedReverseGreeting gret = new ThreadedReverseGreeting();
+		gret.createThread(50);
+	}
+
+	void createThread(int x) {
+
+		System.out.println("Hello from thread " + x + "!");
+
+		for (int i = 0; i < 50; i++) {
+			int n = i;
+			new Thread(() -> createThread(50 - n)).start();
+
+		}
+	}
 }
